3ce8eac33a9cd4bb835a68b4aae1536f391f52e4
Seth Theriault <seththeriault@gmail.com>
2022-02-02 20:36:45 -0500

    libtree: update to 3.0.2

--- Formula/libtree-direct.rb
+++ Formula/libtree-direct.rb
@@ -1,7 +1,7 @@
 class LibtreeDirect < Formula
   desc "ldd as a tree"
   homepage "https://github.com/haampie/libtree"
-  version "3.0.1"
+  version "3.0.2"
   license "MIT"
 
   on_macos do
@@ -10,8 +10,8 @@ class LibtreeDirect < Formula
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/haampie/libtree/releases/download/v3.0.1/libtree_x86_64"
-      sha256 "64b573b5cfadd584c2467bfa50614c78100a83088698ba96b034b17347f4bb28"
+      url "https://github.com/haampie/libtree/releases/download/v3.0.2/libtree_x86_64"
+      sha256 "9f29b7f0a737034114bb13c8779b743d838616d517cd8a2bae54565e9b4d1f7e"
 
       def install
         bin.install "libtree_x86_64" => "libtree"
