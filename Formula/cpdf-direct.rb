61e133b05f64649c7f59bf1e6b8c4028c8ab0989
Seth Theriault <seththeriault@gmail.com>
2022-03-04 16:39:40 -0500

    cpdf: update to 2.5.1

--- Formula/cpdf-direct.rb
+++ Formula/cpdf-direct.rb
@@ -1,20 +1,20 @@
 class CpdfDirect < Formula
   desc "Coherent PDF CLI tools binaries"
   homepage "https://github.com/coherentgraphics/cpdf-binaries"
-  version "2.5"
+  version "2.5.1"
 
   on_macos do
     conflicts_with "cpdf", because: "you should try the version in oncletom/brew first"
     if Hardware::CPU.intel?
       url "https://github.com/coherentgraphics/cpdf-binaries/raw/master/OSX-Intel/cpdf"
-      sha256 "3cdb2c5ddd0127c6a1906a753a639c6d950e93b41e81fceb2cc6473177ab2257"
+      sha256 "d82be8c8eff64b11a67879f63c7a5742ac34d035a71c5799c8714d9b9a96ba4d"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
       url "https://raw.githubusercontent.com/coherentgraphics/cpdf-binaries/master/Linux-Intel-64bit/cpdf"
-      sha256 "11d06458df9b0ca86770017d7569d4c42337d22558add0787312a417f9aa9ae2"
+      sha256 "4e6e5592afe50cd5f9bac404331fae31d2a86d950001bbb05d867855fadf746b"
     end
   end
 
