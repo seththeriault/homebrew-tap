517f3146d9369e1cc9551bee21bd0693537c081c
Seth Theriault <seththeriault@gmail.com>
2022-03-27 19:21:15 -0400

    yt-dlp: update to 2022.03.08.1

--- Formula/yt-dlp-direct.rb
+++ Formula/yt-dlp-direct.rb
@@ -2,8 +2,8 @@ class YtDlpDirect< Formula
 
   desc "A youtube-dl fork with additional features and fixes"
   homepage "https://github.com/yt-dlp/yt-dlp"
-  url "https://github.com/yt-dlp/yt-dlp/releases/download/2022.02.04/yt-dlp"
-  sha256 "a16fe3b3bd474d562c4b8645579b209377b967d58d4edffe6e31dc8de81d7283"
+  url "https://github.com/yt-dlp/yt-dlp/releases/download/2022.03.08.1/yt-dlp"
+  sha256 "2c3dfddeddfe6e0b73dcd77db83bd9b05c5d567f156bab4b256092af07fad5b0"
   license "Unlicense"
 
   def install
