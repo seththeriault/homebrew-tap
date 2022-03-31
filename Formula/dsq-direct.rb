92d1c517ce978bdf64814acbf44368910c50dbab
Seth Theriault <seththeriault@gmail.com>
2022-03-31 09:07:01 -0400

    dsq: update to 0.10.0

--- Formula/dsq-direct.rb
+++ Formula/dsq-direct.rb
@@ -1,12 +1,13 @@
 class DsqDirect < Formula
   desc "CLI tool for running SQL queries against JSON, CSV, Excel, Parquet, and more"
   homepage "https://github.com/multiprocessio/dsq"
+  version "0.10.0"
   license "Apache-2.0"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/multiprocessio/dsq/releases/download/0.9.0/dsq-darwin-x64-0.9.0.zip"
-      sha256 "413356214315c90d36dfe9b976f4bedb4b45569090fd2c0e298ed1bfd6dbf220"
+      url "https://github.com/multiprocessio/dsq/releases/download/0.10.0/dsq-darwin-x64-0.10.0.zip"
+      sha256 "8dffaca1469326ef640d4fd0554a25a019c0bb5508cdf799381706b09d040a75"
     end
   end
 
@@ -22,7 +23,7 @@ class DsqDirect < Formula
 
   test do
     resource("$HOME/userdata.json") do
-      url "https://raw.githubusercontent.com/multiprocessio/dsq/0.5.0/testdata/userdata.json"
+      url "https://raw.githubusercontent.com/multiprocessio/dsq/0.10.0/testdata/userdata.json"
       sha256 "6713173f834d9398271554f8be9f2e431d51cdc58f6804cc3f4f725da28ad73e"
     end
 
