98a1204eeac126fed194e2391488d81e7362b49a
Seth Theriault <seththeriault@gmail.com>
2022-04-21 18:52:12 -0400

    dsq: restrict installs to MacOS

--- Formula/dsq-direct.rb
+++ Formula/dsq-direct.rb
@@ -1,8 +1,11 @@
 class DsqDirect < Formula
   desc "CLI tool for running SQL queries against JSON, CSV, Excel, Parquet, and more"
   homepage "https://github.com/multiprocessio/dsq"
-  version "0.15.1"
+  url "https://github.com/multiprocessio/dsq/archive/refs/tags/0.15.1.tar.gz"
+  sha256 "e8d24d3f231c712a042d1f090ff19a50c8fecafe5a7bd896dacf824aa0dd5b28"
   license "Apache-2.0"
+
+  depends_on :macos
 
   on_macos do
     if Hardware::CPU.intel?
@@ -12,7 +15,7 @@ class DsqDirect < Formula
   end
 
   on_linux do
-    odie "Use homebrew-core formula instead."
+    opoo "Requires glibc 2.28 or greater. Use homebrew-core formula instead or see #{homepage} for more information."
   end
 
   conflicts_with "dsq", because: "you should try the version in homebrew-core first"
