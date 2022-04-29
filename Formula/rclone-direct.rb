8da35b32e405ec639c98c884bc3b912de92f53a5
Seth Theriault <seththeriault@gmail.com>
2022-04-29 19:07:50 -0400

    rclone: update to 1.58.1

--- Formula/rclone-direct.rb
+++ Formula/rclone-direct.rb
@@ -1,20 +1,20 @@
 class RcloneDirect < Formula
   desc "Rsync for cloud storage"
   homepage "https://rclone.org/"
-  version "1.58.0"
+  version "1.58.1"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/rclone/rclone/releases/download/v1.58.0/rclone-v1.58.0-osx-amd64.zip"
-      sha256 "533285c177f817601c35476ccbb9698e431dd750bb73204b51d01bf629846fac"
+      url "https://github.com/rclone/rclone/releases/download/v1.58.1/rclone-v1.58.1-osx-amd64.zip"
+      sha256 "03b104accc26d5aec14088c253ea5a6bba3263ae00fc403737cabceecad9eae9"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/rclone/rclone/releases/download/v1.58.0/rclone-v1.58.0-linux-amd64.zip"
-      sha256 "586553898cc1e9e1f3198d7a0c5d84a34ca4709a35013954a3e648f09e65aa37"
+      url "https://github.com/rclone/rclone/releases/download/v1.58.1/rclone-v1.58.1-linux-amd64.zip"
+      sha256 "135a4a0965cb58eafb07941f2013a82282c44c28fea9595587778e969d9ed035"
     end
   end
 
