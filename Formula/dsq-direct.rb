9c834018520a70058d22d5751f68715c3164216a
Seth Theriault <seththeriault@gmail.com>
2022-07-18 23:09:35 -0400

    dsq: update to 0.21.0

--- Formula/dsq-direct.rb
+++ Formula/dsq-direct.rb
@@ -1,16 +1,16 @@
 class DsqDirect < Formula
   desc "CLI tool for running SQL queries against JSON, CSV, Excel, Parquet, and more"
   homepage "https://github.com/multiprocessio/dsq"
-  url "https://github.com/multiprocessio/dsq/archive/refs/tags/0.17.0.tar.gz"
-  sha256 "e180459c996afa0963497a87ce6f7d7695cbd7ce6ff18067be57436f2b5e6f16"
+  url "https://github.com/multiprocessio/dsq/archive/refs/tags/0.21.0.tar.gz"
+  sha256 "d52fb150908f06bc5d0c468cd771c515429e1ddce66375e41c9c374cb20aca01"
   license "Apache-2.0"
 
   depends_on :macos
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/multiprocessio/dsq/releases/download/0.17.0/dsq-darwin-x64-0.17.0.zip"
-      sha256 "e180459c996afa0963497a87ce6f7d7695cbd7ce6ff18067be57436f2b5e6f16"
+      url "https://github.com/multiprocessio/dsq/releases/download/0.21.0/dsq-darwin-x64-0.21.0.zip"
+      sha256 "9849a7b4aa1bff4afa28892187e21291456165545e6912f8a71efef7a7933eff"
     end
   end
 
@@ -26,7 +26,7 @@ class DsqDirect < Formula
 
   test do
     resource("$HOME/userdata.json") do
-      url "https://raw.githubusercontent.com/multiprocessio/dsq/0.17.0/testdata/userdata.json"
+      url "https://raw.githubusercontent.com/multiprocessio/dsq/0.21.0/testdata/userdata.json"
       sha256 "6713173f834d9398271554f8be9f2e431d51cdc58f6804cc3f4f725da28ad73e"
     end
 
