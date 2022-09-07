ab0b916bffdcc6aed72cc871595e3db8c6557cbb
Seth Theriault <seththeriault@gmail.com>
2022-09-06 21:28:08 -0400

    yq: update to 4.27.3

--- Formula/yq-direct.rb
+++ Formula/yq-direct.rb
@@ -1,13 +1,13 @@
 class YqDirect < Formula
   desc "Process YAML documents from the CLI"
   homepage "https://github.com/mikefarah/yq"
-  version "4.26.1"
+  version "4.27.3"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/mikefarah/yq/releases/download/v4.26.1/yq_darwin_amd64.tar.gz"
-      sha256 "2ce79cff617258e9ee6be51070714f0e1da9bdbd9046507dd935239d17693cb9"
+      url "https://github.com/mikefarah/yq/releases/download/v4.27.3/yq_darwin_amd64.tar.gz"
+      sha256 "60fc3bba1b084d696d8ba550a72687b0254d06fb3b044fe66c5561e6c1b8c8ed"
       $yq_binary = "yq_darwin_amd64"
     end
 
@@ -15,8 +15,8 @@ class YqDirect < Formula
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/mikefarah/yq/releases/download/v4.26.1/yq_linux_amd64.tar.gz"
-      sha256 "4d3afe5ddf170ac7e70f4c23eea2969eca357947b56d5d96b8516bdf9ce56577"
+      url "https://github.com/mikefarah/yq/releases/download/v4.27.3/yq_linux_amd64.tar.gz"
+      sha256 "e6815ada83b65d9c38dd211f08f9f99c77bac72e7ecf5e82a4cf1e8fb314b0ab"
       $yq_binary = "yq_linux_amd64"
     end
 
