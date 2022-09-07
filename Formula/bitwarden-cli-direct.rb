54c0f07ac726fe02758a7af2a13790b4eef5c95e
Seth Theriault <seththeriault@gmail.com>
2022-09-06 21:16:46 -0400

    bitwarden-cli: update to cli-v2022.8.0

--- Formula/bitwarden-cli-direct.rb
+++ Formula/bitwarden-cli-direct.rb
@@ -1,21 +1,21 @@
 class BitwardenCliDirect < Formula
   desc "Secure and free password manager for all of your devices"
   homepage "https://bitwarden.com/"
-  version "cli-v2022.6.2"
+  version "cli-v2022.8.0"
   license "unknown"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/bitwarden/clients/releases/download/cli-v2022.6.2/bw-macos-2022.6.2.zip"
-      sha256 "4717258d7902b416055598313840c0e6edfd9f7015071f8a9b5b86f0a3165320"
+      url "https://github.com/bitwarden/clients/releases/download/cli-v2022.8.0/bw-macos-2022.8.0.zip"
+      sha256 "78866efb3cf6dd61392cb39629a5cb0004b652a92ce96f335ad24bfabbfd5e8c"
     end
 
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/bitwarden/clients/releases/download/cli-v2022.6.2/bw-linux-2022.6.2.zip"
-      sha256 "7748f7eba801c340a53c9c84c3f83cf369f461561276a2d612fefd163c90a69b"
+      url "https://github.com/bitwarden/clients/releases/download/cli-v2022.8.0/bw-linux-2022.8.0.zip"
+      sha256 "fe22d03c011f5d45ce94e1367096cc5bf464ec47665dd368764fa099b9c91a16"
     end
 
   end
