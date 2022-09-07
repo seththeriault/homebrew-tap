0d8313808f583d74e68c7858a171ea625cb2d3d4
Seth Theriault <seththeriault@gmail.com>
2022-09-06 21:10:49 -0400

    usql: update to 0.12.13

--- Formula/usql-direct.rb
+++ Formula/usql-direct.rb
@@ -1,13 +1,13 @@
 class UsqlDirect < Formula
   desc "Universal command-line interface for SQL databases"
   homepage "https://github.com/xo/usql"
-  version "0.11.0"
+  version "0.12.13"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/xo/usql/releases/download/v0.11.0/usql-0.11.0-darwin-amd64.tar.bz2"
-      sha256 "0ad91ad6df7e51b89305a2e8c599ac5d33ead93168f941a334518bd7dab92f04"
+      url "https://github.com/xo/usql/releases/download/v0.12.13/usql-0.12.13-darwin-amd64.tar.bz2"
+      sha256 "c24614025acac0fdd009f0d314d03108674a8bbde00f46063f6682f1a779ba56"
       $usql_binary = "usql"
     end
 
@@ -15,8 +15,8 @@ class UsqlDirect < Formula
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/xo/usql/releases/download/v0.11.0/usql_static-0.11.0-linux-amd64.tar.bz2"
-      sha256 "42ac71980cd1ec83758cdde52724ddd92dec1d24f7d75531a13a584d9680d1ba"
+      url "https://github.com/xo/usql/releases/download/v0.12.13/usql_static-0.12.13-linux-amd64.tar.bz2"
+      sha256 "9efd796fcd6a3d086ab26fed898d1fe5189a0551d707b4dc1b1e17174d77aef5"
       $usql_binary = "usql_static"
     end
 
