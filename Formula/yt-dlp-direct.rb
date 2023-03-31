da0bdacac8569d13c0a4c1776d2ba5fca32e84ff
Seth Theriault <seththeriault@gmail.com>
2023-03-31 08:35:50 -0400

    yt-dlp: link to correct 2023.03.04 binary

--- Formula/yt-dlp-direct.rb
+++ Formula/yt-dlp-direct.rb
@@ -2,7 +2,7 @@ class YtDlpDirect< Formula
 
   desc "A youtube-dl fork with additional features and fixes"
   homepage "https://github.com/yt-dlp/yt-dlp"
-  url "https://github.com/yt-dlp/yt-dlp/releases/download/2023.03.04/yt-dlp.exe"
+  url "https://github.com/yt-dlp/yt-dlp/releases/download/2023.03.04/yt-dlp"
   sha256 "91cad9f121c1f6f0a81b747415c46ecba0ff331ed38cc6433040b4ac7b6e15ca"
   license "Unlicense"
 
