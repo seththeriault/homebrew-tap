bf0acd575d4d8fb5b1705ad76aa9a7e78f893d83
Seth Theriault <seththeriault@gmail.com>
2023-10-01 18:13:46 -0400

    bitwarden-cli: update to cli-v2023.4.0

--- Formula/bitwarden-cli-direct.rb
+++ Formula/bitwarden-cli-direct.rb
@@ -1,21 +1,21 @@
 class BitwardenCliDirect < Formula
   desc "Secure and free password manager for all of your devices"
   homepage "https://bitwarden.com/"
-  version "cli-v2022.10.0"
+  version "cli-v2023.4.0"
   license "unknown"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/bitwarden/clients/releases/download/cli-v2022.10.0/bw-macos-2022.10.0.zip"
-      sha256 "c604a09841435f6388fffc13cb6f772a777041ff2759d24c24e7e7c0687e62de"
+      url "https://github.com/bitwarden/clients/releases/download/cli-v2023.4.0/bw-macos-2023.4.0.zip"
+      sha256 "2e973a95cfd5e5c7dcfb53622ace3bfb39b61086a8ba68fa98736f70f457b6f8"
     end
 
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/bitwarden/clients/releases/download/cli-v2022.10.0/bw-linux-2022.10.0.zip"
-      sha256 "3a47014de6842264b7d0cdd134e54c7b7e8461e7d353814a8b36f1f4ffc9d997  "
+      url "https://github.com/bitwarden/clients/releases/download/cli-v2023.4.0/bw-linux-2023.4.0.zip"
+      sha256 "7333bf62c228f3347bda1d7c00fb34749df6095b923554726b7c7ff97aa096b7"
     end
 
   end
