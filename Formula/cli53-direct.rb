e333f68b4ee0ce1c2bfc65d41996f5d01390a340
Seth Theriault <seththeriault@gmail.com>
2024-01-09 18:40:08 -0500

    cli53: update to 0.8.22

--- Formula/cli53-direct.rb
+++ Formula/cli53-direct.rb
@@ -1,13 +1,13 @@
 class Cli53Direct < Formula
   desc "Command-line tool for Amazon Route 53"
   homepage "https://github.com/barnybug/cli53"
-  version "0.8.18"
+  version "0.8.22"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/barnybug/cli53/releases/download/0.8.18/cli53-mac-amd64"
-      sha256 "951afde3bfdc43f6398729e6aba32b37fd99aad7dba8243494987cfb66d99320"
+      url "https://github.com/barnybug/cli53/releases/download/0.8.22/cli53-mac-amd64"
+      sha256 "260f9164cb843cb6310be3102b114cf770f9407fbf2e01e1ef6758996874a418"
 
       def install
         bin.install "cli53-mac-amd64" => "cli53"
@@ -18,8 +18,8 @@ class Cli53Direct < Formula
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/barnybug/cli53/releases/download/0.8.18/cli53-linux-amd64"
-      sha256 "9afe49aef24e81bdc15f1d57f1e1cbf9f77326b33a2ff376e65da6a7d8ac69e3"
+      url "https://github.com/barnybug/cli53/releases/download/0.8.22/cli53-linux-amd64"
+      sha256 "8134c2964ecd6fa119552f5a67b31fc984f6afdc3a404001a5f22a0a3b9cc7e4"
 
       def install
         bin.install "cli53-linux-amd64" => "cli53"
