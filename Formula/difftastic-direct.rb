dcb8424c7be92e3df7f206dc5d76dfe9cf4c0d9f
Seth Theriault <seththeriault@gmail.com>
2024-03-28 17:40:03 -0400

    difftastic: update to 0.56.1

--- Formula/difftastic-direct.rb
+++ Formula/difftastic-direct.rb
@@ -1,20 +1,20 @@
 class DifftasticDirect < Formula
   desc "Diff that understands syntax"
   homepage "https://github.com/Wilfred/difftastic"
-  version "0.55.0"
+  version "0.56.1"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/Wilfred/difftastic/releases/download/0.55.0/difft-x86_64-apple-darwin.tar.gz"
-      sha256 "4a612c1a53eb70dd8906c989ae1bf12db79abbe2aa46b24fbc8e6ae9d6d34078"
+      url "https://github.com/Wilfred/difftastic/releases/download/0.56.1/difft-x86_64-apple-darwin.tar.gz"
+      sha256 "289d968397637af1cb954f356b51cf3c55a1543d888859d2f5afdfabea291709"
     end
 
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/Wilfred/difftastic/releases/download/0.55.0/difft-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "cd4f3176464dd337c56d323b48a248c1da09a4ab630fbdb42fa3d9baa25b2e2d"
+      url "https://github.com/Wilfred/difftastic/releases/download/0.56.1/difft-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "2922fd79d294afb159e45ff1bc61e8951cd4e239ecfff668824f7f051397a14d"
     end
 
   end
