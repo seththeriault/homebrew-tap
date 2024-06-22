698c49da57a595250a5c4b848c7b868aee9e808e
Seth Theriault <seththeriault@gmail.com>
2024-06-22 16:27:52 -0400

    ripsecrets: update to 0.1.8

--- Formula/ripsecrets-direct.rb
+++ Formula/ripsecrets-direct.rb
@@ -1,21 +1,21 @@
 class RipsecretsDirect < Formula
   desc "A command-line tool to prevent committing secret keys into your source code"
   homepage "https://github.com/sirwart/ripsecrets"
-  url "https://github.com/sirwart/ripsecrets/archive/refs/tags/v0.1.5.tar.gz"
-  sha256 "1e3d36b3892d241dfd5e9abd86ddb47f22e6837b89cf9ee44989d6c1271dda2b"
+  url "https://github.com/sirwart/ripsecrets/archive/refs/tags/v0.1.8.tar.gz"
+  sha256 "4d7209605d3babde73092fed955628b0ecf280d8d68633b9056d2f859741109d"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/sirwart/ripsecrets/releases/download/v0.1.5/ripsecrets-0.1.5-x86_64-apple-darwin.tar.gz"
-      sha256 "0a4458602a14fb5dae2adc07bc2cf7c68e85c101831ec459612a7471b9a951cf"
+      url "https://github.com/sirwart/ripsecrets/releases/download/v0.1.8/ripsecrets-0.1.8-x86_64-apple-darwin.tar.gz"
+      sha256 "7c33a07023c7607ed2c77469d05c12bc5766f1b5a79278d0cbf137b627908fc1"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/sirwart/ripsecrets/releases/download/v0.1.5/ripsecrets-0.1.5-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "4d1cec381a218541bfe1a499bacf80d49319c58b0b107d7da322be60a681b00c"
+      url "https://github.com/sirwart/ripsecrets/releases/download/v0.1.8/ripsecrets-0.1.8-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "726bc64d6a06651f068ceba1ea09e727e12e0d2857dfc132d6838b8b958f5c0b"
     end
   end
 
