2c84d0ef83d26243fb3fb8065fdb1b47b0a0e631
Seth Theriault <seththeriault@gmail.com>
2025-03-21 17:40:02 -0400

    yt-dlp: update to 2025.02.19

--- Formula/yt-dlp-direct.rb
+++ Formula/yt-dlp-direct.rb
@@ -2,12 +2,23 @@ class YtDlpDirect< Formula
 
   desc "A youtube-dl fork with additional features and fixes"
   homepage "https://github.com/yt-dlp/yt-dlp"
-  url "https://github.com/yt-dlp/yt-dlp/releases/download/2024.07.09/yt-dlp"
-  sha256 "cdbda4bd7c73f3da8aee92596394bfb957e86fcc1b7748024104c3775e73c5de"
+  url "https://github.com/yt-dlp/yt-dlp/releases/download/2025.02.19/yt-dlp"
+  sha256 "a18843c75b04756ed1d8e261b54de8b7ddf918f73134175d5acab745455dcbc8"
   license "Unlicense"
 
+  $ytldlp_binary = "yt-dlp"
+
+  on_linux do
+    if Hardware::CPU.intel?
+      url "https://github.com/yt-dlp/yt-dlp/releases/download/2025.02.19/yt-dlp_linux"
+      sha256 "a3e45133e1960a2ecc3c575b8470ab0d48a52bd92eb1ee3b4b82698fb9a2fc48"
+      $ytldlp_binary = "yt-dlp_linux"
+    end
+
+  end
+
   def install
-    bin.install "yt-dlp"
+    bin.install "#$ytldlp_binary" => "yt-dlp"
   end
 
   conflicts_with "yt-dlp", because: "you should try the version in homebrew-core first"
