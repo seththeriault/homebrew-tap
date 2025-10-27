6971acc665f0cc4f73a175838a24ac7a7afe4ef0
Seth Theriault <seththeriault@gmail.com>
2025-10-27 18:21:32 -0400

    certinfo: update to 1.0.38

--- Formula/certinfo-direct.rb
+++ Formula/certinfo-direct.rb
@@ -1,20 +1,20 @@
 class CertinfoDirect < Formula
   desc "Print x509 certificate info."
   homepage "https://github.com/pete911/certinfo"
-  version "1.0.37"
+  version "1.0.38"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/pete911/certinfo/releases/download/v1.0.37/certinfo_1.0.37_darwin_amd64.tar.gz"
-      sha256 "5a4ab275559efa8967dc3390cdc21de0ee41b53eec3fea63adfed654403ea3d0"
+      url "https://github.com/pete911/certinfo/releases/download/v1.0.38/certinfo_1.0.38_darwin_amd64.tar.gz"
+      sha256 "cc46eba3ef2aa5e01d8d1c7e0f9d3530bb18d6c9310cd751da60d98b8466d760"
 
       def install
         bin.install "certinfo"
       end
     end
     if Hardware::CPU.arm?
-      url "https://github.com/pete911/certinfo/releases/download/v1.0.37/certinfo_1.0.37_darwin_arm64.tar.gz"
-      sha256 "3174c8b0b534056b4498b4cabd4d1bbe9b1e44873a1d0be25a37f7b13c117e5b"
+      url "https://github.com/pete911/certinfo/releases/download/v1.0.38/certinfo_1.0.38_darwin_arm64.tar.gz"
+      sha256 "233405552528009524e1d170850f4744cd1abba9183b1f8ec45e7554e3fe83cd"
 
       def install
         bin.install "certinfo"
@@ -25,7 +25,7 @@ class CertinfoDirect < Formula
   on_linux do
     if Hardware::CPU.intel?
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/pete911/certinfo/releases/download/v1.0.37/certinfo_1.0.37_linux_amd64.tar.gz"
+        url "https://github.com/pete911/certinfo/releases/download/v1.0.38/certinfo_1.0.38_linux_amd64.tar.gz"
         sha256 "3380476b65bb97b43a272c748c445aa783746ddc1014f75e0a9161528f0524f8"
 
         def install
@@ -35,8 +35,8 @@ class CertinfoDirect < Formula
     end
     if Hardware::CPU.arm?
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/pete911/certinfo/releases/download/v1.0.37/certinfo_1.0.37_linux_arm64.tar.gz"
-        sha256 "5851281ee445f9b915711665930cda3f32be605bcb84b9c075a8e2c66fe4887f"
+        url "https://github.com/pete911/certinfo/releases/download/v1.0.38/certinfo_1.0.38_linux_arm64.tar.gz"
+        sha256 "aa7cd2fdd234bcf9d85e002cb067a0996a50395da520359fe4caa34693051223"
 
         def install
           bin.install "certinfo"
@@ -44,8 +44,6 @@ class CertinfoDirect < Formula
       end
     end
   end
-
-  conflicts_with "certinfo", because: "you should try the version in pete911 first"
 
   test do
     assert_match /Usage/, shell_output("#{bin}/certinfo -h", 0)
