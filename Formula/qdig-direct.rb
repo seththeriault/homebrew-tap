1da34f52d7ae84657c6cc467cde38a52a8ef4b4f
Seth Theriault <seththeriault@gmail.com>
2025-11-08 11:28:39 -0500

    qdig (ex-qdns): update to 0.19.10

--- Formula/qdig-direct.rb
+++ Formula/qdig-direct.rb
@@ -1,25 +1,25 @@
 class QdigDirect < Formula
   desc "A tiny command line DNS client with support for UDP, TCP, DoT, DoH, DoQ and ODoH."
   homepage "https://github.com/natesales/q"
-  version "0.19.2"
+  version "0.19.10"
   license "GPL-3.0-only"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/natesales/q/releases/download/v0.19.2/q_0.19.2_darwin_arm64.tar.gz"
-      sha256 "e52507b2ef48feaee762f3551b137e501c9ece9f6788129b4f49ffdc3ebd28d6"
+      url "https://github.com/natesales/q/releases/download/v0.19.10/q_0.19.10_darwin_arm64.tar.gz"
+      sha256 "fac9a07cd949988f3797089ee352b0a297f354c2ef953a72c0da4564b0e40a50"
     end
 
     if Hardware::CPU.intel?
-      url "https://github.com/natesales/q/releases/download/v0.19.2/q_0.19.2_darwin_amd64.tar.gz"
-      sha256 "1a3b71d0eb7943f61a983115a79d2b8178ec66d3577430e60662d8e58950762a"
+      url "https://github.com/natesales/q/releases/download/v0.19.10/q_0.19.10_darwin_amd64.tar.gz"
+      sha256 "a9a49f48f61be0ffd4cf931e5b9e4030d73a599082afd64bdfc9dd2eee0ce651"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/natesales/q/releases/download/v0.19.2/q_0.19.2_linux_amd64.tar.gz"
-      sha256 "e3a0a960592d320eddbef8c4ce2f9df4651fbcb0d9648aa24fbf8f0d7b3aace1"
+      url "https://github.com/natesales/q/releases/download/v0.19.10/q_0.19.10_linux_amd64.tar.gz"
+      sha256 "415c12186ba205054a554e6abf0d57ab0712ba9aef64046d6e3dc4ce774db6b6"
     end
   end
 
