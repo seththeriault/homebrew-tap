c58f8faeb9c3bf382403d79aeced93685acbdf0c
Seth Theriault <seththeriault@gmail.com>
2025-11-11 19:36:52 -0500

    fix formula for xleak

--- Formula/xleak-direct.rb
+++ Formula/xleak-direct.rb
@@ -9,7 +9,6 @@ class XleakDirect < Formula
       sha256 "bf5cb5b20ea15a2525eb20544d03b70f571add8cc0c0ea7ba5b36b00ffcd5c72"
     end
 
-  on_macos do
     if Hardware::CPU.intel?
       url "https://github.com/bgreenwell/xleak/releases/download/v0.1.0/xleak-macos-x86_64.tar.gz"
       sha256 "a44cdde53782775a58faaceb5b8e14f0efdb301e12fdf19f5ce3d8a0dfe22d3d"
